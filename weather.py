7102e079dcf08dc19547e3e41cd6658531ef32a3
Maxwell Roselius <mroseliu@figs.cs.colostate.edu>
2016-08-15 17:14:11 -0600

    removed comment

--- weather.py
+++ weather.py
@@ -4,8 +4,8 @@ import urllib2
 import math
 import pandas as pd
 
-v_comp = 'http://nomads.ncdc.noaa.gov/thredds/ncss/grid/ndgd/201605/20160525/LVIA98_KWBR_201605251900?var=V-component_of_wind&latitude=42.123587546&longitude=-71.13572160735384&time_start=2013-05-25T18%3A00%3A00Z&time_end=2013-05-25T18%3A00%3A00Z&temporal=point&time=2013-05-25T18%3A00%3A00Z&vertCoord=0&accept=csv&point=true'
-u_comp = 'http://nomads.ncdc.noaa.gov/thredds/ncss/grid/ndgd/201605/20160525/LUIA98_KWBR_201605251900?var=U-component_of_wind&latitude=42.123587546&longitude=-71.13572160735384&time_start=2013-05-25T18%3A00%3A00Z&time_end=2013-05-25T18%3A00%3A00Z&temporal=point&time=2013-05-25T18%3A00%3A00Z&vertCoord=0&accept=csv&point=true'
+v_comp = 'http://nomads.ncdc.noaa.gov/thredds/ncss/grid/ndgd/201605/20160525/LVIA98_KWBR_201605251900?var=V-component_of_wind&latitude=41.123587546&longitude=-70.13572160735384&time_start=2013-05-25T18%3A00%3A00Z&time_end=2013-05-25T18%3A00%3A00Z&temporal=point&time=2013-05-25T18%3A00%3A00Z&vertCoord=0&accept=csv&point=true'
+u_comp = 'http://nomads.ncdc.noaa.gov/thredds/ncss/grid/ndgd/201605/20160525/LUIA98_KWBR_201605251900?var=U-component_of_wind&latitude=41.123587546&longitude=-70.13572160735384&time_start=2013-05-25T18%3A00%3A00Z&time_end=2013-05-25T18%3A00%3A00Z&temporal=point&time=2013-05-25T18%3A00%3A00Z&vertCoord=0&accept=csv&point=true'
 v = urllib2.urlopen(v_comp)
 u = urllib2.urlopen(u_comp)
 cr = csv.reader(v)
@@ -18,7 +18,7 @@ U = uRead['U-component_of_wind[unit="m s-1"]'].iloc[0]
 DperR = 180.0/math.pi
 # print DperR
 direction = 270 - (math.atan2(V,U) * DperR)
-# speed = math.sqrt(U**2+V**2)
+speed = math.sqrt(U**2+V**2)
 print direction
 if direction>0 and direction<=30:
 	direction = "N"
@@ -39,6 +39,6 @@ elif direction>300 and direction<=330:
 elif direction>330:
 	direction = "N"
 
-	
+
 print 'Wind direction: ' + str(direction)
 print 'Wind speed: ' + str(speed)
